eb6c159cad44a48faa587396439c275c0c8d5e78
mfks17 <mfks17@gmail.com>
2016-05-17 18:15:53 +0900

    add propen
    ref:http://qiita.com/laiso/items/8a30e3656c980863ccfa

--- custom-zshrc.zsh
+++ custom-zshrc.zsh
@@ -24,3 +24,10 @@ function peco-src () {
 }
 zle -N peco-src
 bindkey '^r' peco-src
+
+# propne
+# http://qiita.com/laiso/items/8a30e3656c980863ccfa
+propen() {
+    local current_branch_name=$(git symbolic-ref --short HEAD | xargs perl -MURI::Escape -e 'print uri_escape($ARGV[0]);')
+    hub browse -- pull/${current_branch_name}
+}
