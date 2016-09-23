3acbef646bd3fa108b16c809e01f370bf0f7f238
mfks17 <mfks17@gmail.com>
2016-09-23 09:29:49 +0900

    :bug: Fix github open alias

--- custom-aliases.zsh
+++ custom-aliases.zsh
@@ -1,6 +1,6 @@
 alias ghg='ghq get'
 alias o='open .'
-alias gho='gh-open .'
+alias gho="open `git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@com:@com/@'`| head -n1"
 alias vi='mvim -v'
 alias gi='gvim'
 alias sl='swiftlint'
